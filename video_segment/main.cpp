0d00bcbf22572df63a47917884fe0cf0b187558c
Yanbo-Zhu <yanbozhu1991@gmail.com>
2017-09-21 22:09:12 +0200

    Initial Commit

---
+++ video_segment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  video_segment
+//
+//  Created by Yanbo Zhu on 21.09.17.
+//  Copyright © 2017 Zhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
