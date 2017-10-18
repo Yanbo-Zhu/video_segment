12b6f7e814d8992cc6d6ab05a8f40558381d959b
Yanbo-Zhu <yanbozhu1991@gmail.com>
2017-10-18 11:38:53 +0200

    3

--- video_segment/main.cpp
+++ video_segment/main.cpp
@@ -30,7 +30,7 @@ Point g_pt;
 //vector<Point> seedvektor;
 //Point regioncenter ;
 
-Mat FramewithCounter;
+//Mat FramewithCounter;
 Mat firstFrame;
 //Mat frame;
 //Mat MatOut;
@@ -48,7 +48,9 @@ clock_t  clockBegin, clockEnd;
 //----- global function
 //void on_MouseHandle(int event, int x, int y, int flags, void* param);
 void DrawLine( Mat& img, Point pt );
-void Counter (Mat MatOut , Mat currentFrame, Vec3b color);
+Mat Counter (Mat MatOut , Mat currentFrame, Vec3b color);
+void drawAxis(Mat&, Point, Point, Scalar, const float);
+double getOrientation(const vector<Point> &, Mat&);
 //Mat RegionGrow(Mat MatIn, Mat MatGrowCur, double iGrowJudge, vector<Point> seedset);
 //Mat RegionGrow(Mat MatIn, Mat MatBlur ,double iGrowJudge, vector<Point> seedset);
 //double differenceValue(Mat MatIn, Point oneseed, Point nextseed, int DIR[][2], double rowofDIR, double B, double G, double R );
@@ -77,8 +79,9 @@ int main( )
     int indexFrame  = vc.get(CV_CAP_PROP_POS_FRAMES);
     int FPS  = vc.get(CV_CAP_PROP_FPS);
     int FRAME_COUNT = vc.get(CV_CAP_PROP_FRAME_COUNT);
-    
-    printf("Fourcc: %d / indexFrame: %d / fps: %d / Frame_amount: %d \n", Fourcc ,indexFrame, FPS, FRAME_COUNT);
+    int Width = vc.get(CV_CAP_PROP_FRAME_WIDTH);
+    int Height = vc.get(CV_CAP_PROP_FRAME_HEIGHT);
+    printf("Fourcc: %d / indexFrame: %d / fps: %d / Frame_amount: %d / Width * Height : %d * %d \n", Fourcc ,indexFrame, FPS, FRAME_COUNT, Width, Height );
     //cout<< Fourcc<< endl;
     
     
@@ -104,7 +107,7 @@ int main( )
     cout<<"choose seeds by clicking in orignal image." <<endl;
     mode = 2;
     
-    cout<<"how many initial segment do you want: " <<endl;
+    cout<<"How many initial segment do you want: " <<endl;
     cin >> Segmentnum;
     
     Initalseed s[Segmentnum];
@@ -185,16 +188,17 @@ int main( )
         //Regiongrowing R1;
         
         Matfinal = frame.clone();
-        FramewithCounter = frame.clone();
+        Mat FramewithCounter = frame.clone();
         
         for( int i=0; i<Segmentnum; i++)
         {
+            printf("Objekt %d Information: \n", i+1);
             MatOut = R[i].RegionGrow(frame, frame_Blur , differencegrow, s[i].initialseedvektor);
             
              s[i].initialseedvektor.clear();
              s[i].initialseedvektor.push_back(R[i].regioncenter);
             
-            Counter(MatOut, frame, color[i]);
+            FramewithCounter = Counter(MatOut, frame, color[i]);
             
             for(size_t j=0; j<R[i].seedtogether.size(); j++)
             {
@@ -204,18 +208,10 @@ int main( )
             
         }
         
-        // ----------------
-        
-
         
         //addWeighted(frame,1, result, 10 ,0, result);
         
         imshow("segment counter", FramewithCounter);
-        
-//        // dilate
-//        int elementSize  = 2;
-//        Mat element = getStructuringElement(MORPH_RECT, Size(2*elementSize+1,2*elementSize+1));
-//        dilate(Matfinal, Matfinal, element);
         
         
 //        Regiongrowing R2;
@@ -276,10 +272,10 @@ int main( )
     return 0;
 }
 // -------
-void Counter (Mat MatOut , Mat currentFrame, Vec3b color)
+Mat Counter (Mat MatOut , Mat currentFrame, Vec3b color)
 {
     Mat MatoutGray;
-    //Mat FramewithCounter = currentFrame.clone();
+    Mat FramemitCounter = currentFrame.clone();
     //Mat result (MatOut.size(),CV_8UC3, Scalar(0,0,0));
     //Mat result = zeros(MatOut.clone();
     
@@ -292,8 +288,6 @@ void Counter (Mat MatOut , Mat currentFrame, Vec3b color)
     
     
     cvtColor(MatOut,MatoutGray,CV_BGR2GRAY);
-    imshow("MatoutGray", MatoutGray);
-    waitKey(10);
     
     threshold(MatoutGray,MatoutGray,20,255,THRESH_BINARY);
     
@@ -308,11 +302,71 @@ void Counter (Mat MatOut , Mat currentFrame, Vec3b color)
         // Ignore contours that are too small or too large
         if (area < 1e2 || 1e5 < area) continue;
         // Draw each contour only for visualisation purposes
-        drawContours(FramewithCounter, contours, static_cast<int>(i), color, 2, 8, hierarchy, 0);
+        drawContours(FramemitCounter, contours, static_cast<int>(i), color, 2, 8, hierarchy, 0);
         //drawContours(result, contours, -1, Scalar(0, 0, 255), 1, 8, hierarchy, 0);
+        getOrientation(contours[i], FramemitCounter);
     }
-    //return FramewithCounter;
+    
+    return FramemitCounter;
 }
+
+
+double getOrientation(const vector<Point> &pts, Mat &img)
+{
+    //Construct a buffer used by the pca analysis
+    int sz = static_cast<int>(pts.size());
+    Mat data_pts = Mat(sz, 2, CV_64FC1);
+    for (int i = 0; i < data_pts.rows; ++i)
+    {
+        data_pts.at<double>(i, 0) = pts[i].x;
+        data_pts.at<double>(i, 1) = pts[i].y;
+    }
+    //Perform PCA analysis
+    PCA pca_analysis(data_pts, Mat(), CV_PCA_DATA_AS_ROW);
+    //Store the center of the object
+    Point cntr = Point(static_cast<int>(pca_analysis.mean.at<double>(0, 0)),
+                       static_cast<int>(pca_analysis.mean.at<double>(0, 1)));
+    //Store the eigenvalues and eigenvectors
+    vector<Point2d> eigen_vecs(2);
+    vector<double> eigen_val(2);
+    for (int i = 0; i < 2; ++i)
+    {
+        eigen_vecs[i] = Point2d(pca_analysis.eigenvectors.at<double>(i, 0),
+                                pca_analysis.eigenvectors.at<double>(i, 1));
+        eigen_val[i] = pca_analysis.eigenvalues.at<double>(0, i);
+    }
+    // Draw the principal components
+    circle(img, cntr, 3, Scalar(255, 0, 255), 2);
+    Point p1 = cntr + 0.02 * Point(static_cast<int>(eigen_vecs[0].x * eigen_val[0]), static_cast<int>(eigen_vecs[0].y * eigen_val[0]));
+    Point p2 = cntr - 0.02 * Point(static_cast<int>(eigen_vecs[1].x * eigen_val[1]), static_cast<int>(eigen_vecs[1].y * eigen_val[1]));
+    drawAxis(img, cntr, p1, Scalar(0, 255, 0), 1);
+    drawAxis(img, cntr, p2, Scalar(255, 255, 0), 5);
+    double angle = atan2(eigen_vecs[0].y, eigen_vecs[0].x); // orientation in radians
+    return angle;
+}
+
+
+void drawAxis(Mat& img, Point p, Point q, Scalar colour, const float scale = 0.2)
+{
+    double angle;
+    double hypotenuse; //直角三角形的斜边
+    angle = atan2( (double) p.y - q.y, (double) p.x - q.x ); // angle in radians
+    hypotenuse = sqrt( (double) (p.y - q.y) * (p.y - q.y) + (p.x - q.x) * (p.x - q.x));
+        double degrees = angle * 180 / CV_PI; // convert radians to degrees (0-180 range)
+        cout << "Degrees: " << abs(degrees - 180) << endl; // angle in 0-360 degrees range
+    // Here we lengthen the arrow by a factor of scale
+    q.x = (int) (p.x - scale * hypotenuse * cos(angle));
+    q.y = (int) (p.y - scale * hypotenuse * sin(angle));
+    line(img, p, q, colour, 1, CV_AA);
+    // create the arrow hooks
+    p.x = (int) (q.x + 9 * cos(angle + CV_PI / 4));
+    p.y = (int) (q.y + 9 * sin(angle + CV_PI / 4));
+    line(img, p, q, colour, 1, CV_AA);
+    p.x = (int) (q.x + 9 * cos(angle - CV_PI / 4));
+    p.y = (int) (q.y + 9 * sin(angle - CV_PI / 4));
+    line(img, p, q, colour, 1, CV_AA);
+}
+
 
 //-------------------------------------- VideoWriter function ----------------
 //waitKey(0);
